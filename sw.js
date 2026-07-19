a8d6490a001078c9c9759ab9dca50b9e286fa1b5
Leandro Melchiori <leandromelchiori@gmail.com>
2026-07-19 15:22:38 -0300

    Corregir error de sintaxis JS y actualizar version de Service Worker

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    ============================================================ */
 
 // Subí este número en cada deploy para invalidar las caches viejas.
-const VERSION   = 'v1.2.0';
+const VERSION   = 'v1.2.1';
 const APP_CACHE = `app-${VERSION}`;       // Núcleo: HTML + iconos + assets propios
 const RUNTIME   = `runtime-${VERSION}`;   // Externos: Google Fonts, CDNs, etc.
 
